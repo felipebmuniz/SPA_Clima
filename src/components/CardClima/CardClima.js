4c6c99cc3f12710cc4896e59db628d5ec73f8b9c
Felipe Barros Muniz <felipemuniz@alu.ufc.br>
2022-01-14 10:48:27 -0300

    Subindo repositório

---
+++ src/components/CardClima/CardClima.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CardClima.module.css';
+
+const CardClima = (props) => {
+  return (
+        <div className={styles.wrapper}>
+          <h1>DADOS DO CLIMA DA CIDADE</h1>
+        </div>
+  )
+}
+
+export default CardClima
---
+++ src/components/Home/Home.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import CardClima from '../CardClima/CardClima';
+import styles from './Home.module.css';
+
+const Home = () => {
+  const [cidade, setCidade] = React.useState('');
+  const [dados, setDados] = React.useState(null);
+
+  function getClimaCidade () {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=3b72f8ef1c537df0d217d2d0249332ef`)
+    .then((response) => response.json())
+    .then(json => {
+      console.log(json);
+      setDados(json)
+    })
+    .catch(err => {
+      // trata se alguma das promises falhar
+      console.error('Failed retrieving information', err);
+    });
+  }
+
+  // React.useEffect(() => {
+  // }, []);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Enviou!!');
+    getClimaCidade();
+    // console.log(dados);
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h2>
+          Clima SPA - Felipe
+        </h2>
+      </header>
+      <main className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label} htmlFor='cidade'>Digite a cidade para efetuar a busca </label>
+          <input className={styles.input} name='cidade'type='text' onChange={({target}) => setCidade(target.value)} />
+          <button className={styles.button}>Buscar</button>
+        </form>
+        {dados &&
+          <CardClima />
+        }
+        
+        </main>
+      
+      <footer className={styles.footer}>
+        <p>Alguns direitos reservados. Felipe Dev.</p>
+      </footer>
+    </>
+  )
+}
+
+export default Home
