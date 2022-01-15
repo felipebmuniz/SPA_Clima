d5558ae77f1acf6d8a27107489120dbdd2b5fff5
Felipe Barros Muniz <felipemuniz@alu.ufc.br>
2022-01-14 21:51:52 -0300

    Finalizado a SPA

--- src/components/CardClima/CardClima.js
+++ src/components/CardClima/CardClima.js
@@ -3,10 +3,15 @@ import styles from './CardClima.module.css';
 
 const CardClima = (props) => {
   return (
-        <div className={styles.wrapper}>
-          <h1>DADOS DO CLIMA DA CIDADE</h1>
-        </div>
-  )
-}
+    <div className={styles.wrapper}>
+      <h1>DADOS DO CLIMA DA CIDADE</h1>
+      <p>Temperatura: {props.dados.main.feels_like} °C</p>
+      <p>Presão atmosférica (nível do mar): {props.dados.main.pressure} hPa</p>
+      <p>Umidade: {props.dados.main.humidity} %</p>
+      <p>Temperatura mínima: {props.dados.main.temp_min} °C</p>
+      <p>Temperatura máxima: {props.dados.main.temp_max} °C</p>
+    </div>
+  );
+};
 
-export default CardClima
+export default CardClima;
